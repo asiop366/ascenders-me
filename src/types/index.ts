6ea3350750f6e3e5d4f79f369efda63ccd988d88
adam <114147515+asiop366@users.noreply.github.com>
2025-12-17 06:52:49 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,21 @@
+import { User, Grade, Space, Channel, Thread, Post } from '@prisma/client'
+
+export type UserWithGrade = User & {
+  grade: Grade | null
+}
+
+export type ThreadWithAuthor = Thread & {
+  author: UserWithGrade
+  _count: {
+    posts: number
+  }
+}
+
+export type PostWithAuthor = Post & {
+  author: UserWithGrade
+}
+
+export type ChannelWithSpace = Channel & {
+  space: Space
+}
 
